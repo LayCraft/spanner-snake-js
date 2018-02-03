18924dcd7c74c33fbc3bb07f42d32846f93e229c
Curtis LayCraft <curtis.laycraft@zoho.com>
2018-02-03 00:04:19 -0800

    Add message for other http requests

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 
 http.createServer((req, res) => {
-    if (req.method !== 'POST') return respond(); // non-game requests
+    if (req.method !== 'POST') return res.end("A Battlesnake server is required to get useful responses from Spanner Snake. curtis.laycraft@zoho.com"); // non-game requests
 
-    let body = [];
+    let body = []; //collector for long http
     req.on('data', chunk => body.push(chunk));
     req.on('end', () => {
         body = JSON.parse(Buffer.concat(body).toString());
