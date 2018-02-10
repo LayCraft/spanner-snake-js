829c3ba4d34a76317cf372108f96c21dd6ea13d0
Curtis LayCraft <curtis.laycraft@zoho.com>
2018-02-10 12:29:34 -0800

    draw board looks right and shows fgh

--- app/draw-board.js
+++ app/draw-board.js
@@ -1,14 +1,24 @@
 var drawBoard = (board) => {
-    console.log('-------Board-------');
-    let output = [];
-    board.forEach(y => {
-        let row = [];
-        y.forEach(x => {
-            row.push(x.f);
-        });
-        output.push(row);
-    });
-    console.log(output);
+
     
+    header = "/";
+    for (let i = 0; i<board[0].length; i++){
+        header=header+'FGH|';
+    }
+    console.log('-------Board-------');
+    //print header line fgh
+    console.log(header);
+
+    board.forEach(y => {
+        let line = "|";
+        y.forEach(x => {
+            // done like this to avoid JS adding elements
+            line = line + `${x.f}${x.g}${x.h}|`;
+        });
+        console.log(line);
+    });
+    let footer = "";
+    for (let i = 0; i<header.length; i++) footer = footer+"-";
+    console.log(footer);
 }
 module.exports = drawBoard;
